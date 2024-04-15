0bb78a0a3f1e5392c6d9acc112ddb09d020f86e9
olijag <oli-jag@wp.pl>
2024-04-16 00:24:52 +0200

    Updated solution 144

--- NeetCode 150/144. Binary Tree Preorder Traversal/solution144.py
+++ NeetCode 150/144. Binary Tree Preorder Traversal/solution144.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/binary-tree-preorder-traversal/
+# Time complexity O(n)
 from typing import List, Optional
 
 # Definition for a binary tree node.
@@ -10,27 +11,25 @@ class TreeNode:
 
 class Solution:
     def preorderTraversal(self, root: TreeNode) -> List[int]:
-        if root is None: # Tree is empty
-            return []
+        if root is None: # List is empty, return []
+            return [] 
 
-        tree_to_list = [root] 
+        root_to_list = [root]
         answer = []
         
-        while tree_to_list:
-            node = tree_to_list.pop()
+        while root_to_list: # Iterate as long as there are objects in root_to_list
+            node = root_to_list.pop() # Get the top of the stack 
             if node:
-                answer.append(node.val)
+                answer.append(node.val) 
                 if node.right:
-                    tree_to_list.append(node.right)
+                    root_to_list.append(node.right) # Append right first because left must be on top of the stack
                 if node.left:
-                    tree_to_list.append(node.left)
+                    root_to_list.append(node.left) # Append left, ensures that left is on top of the stack
         
         return answer
-
-
-
+    
 def main() -> None:
-    test_case = []
+    test_case = [1,2,3,4,5]
     solution = Solution()
     print(solution.preorderTraversal(test_case))
 
