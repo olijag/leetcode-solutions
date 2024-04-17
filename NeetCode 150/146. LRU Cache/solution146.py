2240a7331f81d5d0e9c3bdfaf2cff3c0cceedaec
olijag <oli-jag@wp.pl>
2024-04-18 00:34:01 +0200

    Code update

--- NeetCode 150/146. LRU Cache/solution146.py
+++ NeetCode 150/146. LRU Cache/solution146.py
@@ -1,14 +1,22 @@
 # https://leetcode.com/problems/lru-cache/description/
 
 class LRUCache:
-
     def __init__(self, capacity: int):
         if capacity <= 1: 
             print("Error: Cache size must be bigger or equal 1")
             return None
+        
+        self.capacity = capacity
 
     def get(self, key: int) -> int:
         pass
 
     def put(self, key: int, value: int) -> None:
         pass
+
+    
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
