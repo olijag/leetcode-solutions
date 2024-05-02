ba7bd42a70b879fcb49335ee8d2d035d6438dc9e
olijag <oli-jag@wp.pl>
2024-05-02 16:34:31 +0200

    First implementation of LRU

--- NeetCode 150/146. LRU Cache/solution146.py
+++ NeetCode 150/146. LRU Cache/solution146.py
@@ -2,17 +2,33 @@
 
 class LRUCache:
     def __init__(self, capacity: int):
-        if capacity <= 1: 
-            print("Error: Cache size must be bigger or equal 1")
-            return None
-        
         self.capacity = capacity
+        self.deque = []
+        self.map = {}
 
     def get(self, key: int) -> int:
-        pass
+        if key not in self.map.keys:
+            return -1
+        
+        return self.deque[self.map[key]]
 
     def put(self, key: int, value: int) -> None:
-        pass
+        # If key is not present in the cache
+        if key not in self.map.keys():
+            # Cache is full
+            if len(self.deque) == self.capacity:
+                # Delete least recently used 
+                last_element = self.deque[-1]
+                pop_last_element = self.deque.pop()
+                del self.map[last_element]
+        
+        # else present in the cache
+        else: 
+            del self.deque[self.map[key]]
+        
+        # Update cache
+        self.deque.insert(0, value)
+        self.map[key] = 0
 
     
 
