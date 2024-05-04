783898d8b2c33237bd714eb47860e5ae7b5566fe
olijag <oli-jag@wp.pl>
2024-05-04 23:00:02 +0200

    Updated solution. Implemented in different version with use of a better data structure OrderedDict

--- NeetCode 150/146. LRU Cache/solution146.py
+++ NeetCode 150/146. LRU Cache/solution146.py
@@ -1,34 +1,33 @@
 # https://leetcode.com/problems/lru-cache/description/
 
+from collections import OrderedDict
+
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.deque = []
-        self.map = {}
+        self.cache = OrderedDict()
 
     def get(self, key: int) -> int:
-        if key not in self.map.keys:
+        # If key is not present in the cache
+        if key not in self.cache:
             return -1
         
-        return self.deque[self.map[key]]
+        self.cache.move_to_end(key)
+        return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
-        # If key is not present in the cache
-        if key not in self.map.keys():
-            # Cache is full
-            if len(self.deque) == self.capacity:
-                # Delete least recently used 
-                last_element = self.deque[-1]
-                pop_last_element = self.deque.pop()
-                del self.map[last_element]
-        
-        # else present in the cache
-        else: 
-            del self.deque[self.map[key]]
-        
-        # Update cache
-        self.deque.insert(0, value)
-        self.map[key] = 0
+        # Key is present in cache
+        if key in self.cache:
+            # Update the existing key's value
+            self.cache[key] = value
+            self.cache.move_to_end(key)
+        # Key is not present in cache
+        else:
+            # Pop last item to make space
+            if len(self.cache) == self.capacity:
+                self.cache.popitem(last=False)
+            # Update cache
+            self.cache[key] = value
 
     
 
